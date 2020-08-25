e27fe37da522fdf964c54d4ee8f189f4add822e7
AmeyaJain-25 <ameiya.jain@hotmail.com>
2020-08-25 19:11:57 +0530

    Corona Tracker API Added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+getData()
+
+
+
+
+
+
+
+
 jQuery(document).ready(function(){
     "use strict"
     $("header").ripples({
@@ -6,3 +15,31 @@ jQuery(document).ready(function(){
     })
 
 });
+
+
+
+
+
+async function getData() {
+    const response = await fetch("https://api.covid19india.org/v4/data.json");
+    const data = await response.json();
+    console.log(data);
+
+
+    const coronaTime = document.querySelector('.corona-time');
+    coronaTime.innerHTML = "Last Updated: " + data.MH.meta.last_updated
+
+    const conf = document.querySelector('.confirmedCases');
+    conf.innerHTML = data.MH.districts.Pune.total.confirmed;
+    let act = document.querySelector('.activeCases');
+    let actCases = data.MH.districts.Pune.total.confirmed - data.MH.districts.Pune.total.recovered - data.MH.districts.Pune.total.deceased;
+    act.innerHTML = actCases;
+    let rec = document.querySelector('.recoveredCases');
+    rec.innerHTML = data.MH.districts.Pune.total.recovered;
+    let dth = document.querySelector('.deathCases');
+    dth.innerHTML = data.MH.districts.Pune.total.deceased;
+
+
+}
+
+
